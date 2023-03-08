44ba6ec9a8534c278f3ecedb3d9aa4d1d7582906
Meghna Pamula <mpamula25@cmc.edu>
2023-03-07 21:48:30 -0800

    minor change!

--- containers/fibonacci.py
+++ containers/fibonacci.py
@@ -1,7 +1,7 @@
 def fibs(n):
     '''
     This function computes the first n
-    fibonacci numbers.
+    fibonacci numbers!
     Notice that this function uses O(n) memory.
     '''
     fibs = []
